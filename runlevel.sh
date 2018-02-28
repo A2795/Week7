3e63fa4177faad9ad89d1b9b3dbf92182551c0b0
A2795 <mussieasrat47@gmail.com>
2018-02-28 05:01:42 +0000

    runlevel

---
+++ runlevel.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+clear
+echo "Script run level"
+# by typing in runlevel
+runlevel
+sleep 2
+echo "Another similar command that whill show your"
+echo "current run level with the date and time"
+echo "is using the command who"
+# run the command who -r
+who -r
